fa845d7216025f0f84198a00d853ee8382f3ffbe
Ciocirdel Georgiana Diana <dianageo93@yahoo.com>
2014-01-11 16:41:32 +0200

    ElevFrame done. Ugly code, ugly gui, but time is short :(

--- src/gui/ElevFrame.java
+++ src/gui/ElevFrame.java
@@ -8,7 +8,6 @@ import gui.utils.UserFrameEquiped;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Event;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
@@ -17,7 +16,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Map;
@@ -27,6 +28,7 @@ import java.util.TreeSet;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -36,9 +38,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableModel;
 
 import liceu.Catalog;
 import liceu.Centralizator;
@@ -47,6 +53,7 @@ import liceu.Elev;
 import liceu.Materie;
 import liceu.Profesor;
 import liceu.SituatieMaterieBaza;
+import liceu.SituatieMaterieBaza.Absenta;
 import liceu.SituatieMaterieBaza.Semestru;
 import liceu.SituatieMaterieCuTeza;
 import liceu.utils.Nota;
@@ -66,6 +73,10 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 	// final in order to be used in Events() -> it requires constant values
 	private final String NOTE_SEMESTRUL_1 = "show note semestrul 1";
 	private final String NOTE_SEMESTRUL_2 = "show note semestrul 2";
+	private final String CALCULEAZA_MEDIE_SEMESTRU = "calculeaza medie semestru";
+	private final String CALCULEAZA_MEDIE_GENERALA = "calculeaza medie generala";
+	private final String ABSENTE_SEMESTRU = "afiseaza absente semestrul";
+	private final String TOTAL_ABSENTE = "calculeaza total absente";
 	
 	public void setMaterieDetailsLabel(BasicLabel materieDetailsLabel) {
 		this.materieDetailsLabel = materieDetailsLabel;
@@ -104,6 +115,12 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 					}
 					else if(labels.indexOf(i) == 2) {
 						setupCatalogPanel();
+					}
+					else if(labels.indexOf(i) == 3) {
+						setupColegiPanel();
+					}
+					else {
+						setupHomePanel();
 					}
 					
 				}
@@ -156,11 +173,43 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 		
 	}
 	
-	public void setupCatalogPanel() {
+	private JPanel clearCenterPanel() {
 		JPanel centerPanel = super.getCenterPanel();
-		centerPanel.setLayout(new FlowLayout());
+		centerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		centerPanel.removeAll();
 		centerPanel.repaint();
+		return centerPanel;
+	}
+	
+	public void setupColegiPanel() {
+		JPanel centerPanel = clearCenterPanel();
+		JTable table = new JTable(new ColegiTableModel());
+        table.setPreferredScrollableViewportSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() - 10));
+        table.setFillsViewportHeight(true);
+        table.setOpaque(false);
+        for(int row = 0; row < table.getRowCount(); row++) {
+        	table.setRowHeight(row, 35);
+        }
+        table.setAutoCreateRowSorter(true);
+        for(int row = 0; row < table.getRowCount(); row++) {
+        	table.setRowHeight(row, 35);
+        }
+        
+        if(scrollPane != null) {
+        	centerPanel.remove(scrollPane);
+        }
+        scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() - 10));
+        scrollPane.getViewport().setBackground(Color.DARK_GRAY);
+        setScrollPane(scrollPane);
+        centerPanel.add(scrollPane);
+        
+        centerPanel.revalidate();
+        centerPanel.repaint();
+	}
+	
+	public void setupCatalogPanel() {
+		JPanel centerPanel = clearCenterPanel();
 		
 		JMenuBar menuBar;
 		JMenu menu, submenu;
@@ -168,7 +217,7 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 		
 		//Create the menu bar.
 		menuBar = new JMenuBar();
-		menuBar.setPreferredSize(new Dimension(centerPanel.getWidth() - 5, 20));
+		menuBar.setPreferredSize(new Dimension(centerPanel.getWidth() - 10, 20));
 
 		//Build the first menu.
 		menu = new JMenu("Situatie generala");
@@ -178,26 +227,22 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 		submenu = new JMenu("Note");
 
 		menuItem = new JMenuItem("Semestrul 1");
-		menuItem.addActionListener(new Events());
+		menuItem.addActionListener(new CustomActionListener());
 		menuItem.setActionCommand(NOTE_SEMESTRUL_1);
 		submenu.add(menuItem);
 
 		menuItem = new JMenuItem("Semestrul 2");
-		menuItem.addActionListener(new Events());
+		menuItem.addActionListener(new CustomActionListener());
 		menuItem.setActionCommand(NOTE_SEMESTRUL_2);
 		submenu.add(menuItem);
 		menu.add(submenu);
 		
 		//another submenu
 		menu.addSeparator();
-		submenu = new JMenu("Absente");
-
-		menuItem = new JMenuItem("Semestrul 1");
-		submenu.add(menuItem);
-
-		menuItem = new JMenuItem("Semestrul 2");
-		submenu.add(menuItem);
-		menu.add(submenu);
+		menuItem = new JMenuItem("Absente");
+		menuItem.addActionListener(new CustomActionListener());
+		menuItem.setActionCommand(ABSENTE_SEMESTRU);
+		menu.add(menuItem);
 		
 		//Build second menu in the menu bar.
 		menu = new JMenu("Materii");
@@ -237,9 +282,20 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 		return scrollPane;
 	}
 	
+	private JButton medieSemBttn = null;
+	private BasicLabel medieSemLbl = null;
+	private JButton medieGenBttn = null;
+	private BasicLabel medieGenLbl = null;
+	private double medieSem = 0.0;
+	private double medieGen = 0.0;
+	
 	public void setupCatalogPanel(Semestru sem) {
 		JPanel centerPanel = super.getCenterPanel();
-		String[] columnNames = { "Semestru", "Materie", "Note", "Nota teza", "Medie" };
+		String[] columnNames = { "Semestru", 
+								 "Materie", 
+								 "Note", 
+								 "Nota teza", 
+								 "Medie" };
 		TreeSet<Clasa> clase = Centralizator.getInstance().getClase();
 		String clasa = Centralizator.getInstance().getElevi().get(super.getUsername()).getClasa();
 		Clasa found = null;
@@ -253,6 +309,10 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 			return;
 		}
 		Catalog ctg = found.getCatalog();
+		
+		medieSem = ctg.getMedieElevSemestru(Centralizator.getInstance().getElevi().get(super.getUsername()), sem);
+		medieGen = ctg.getMedieGeneralaElev(Centralizator.getInstance().getElevi().get(super.getUsername()));
+		
 		TreeMap<Materie, SituatieMaterieBaza> sit = found.getCatalog().getMyMap().
 				get(Centralizator.getInstance().getElevi().get(getUsername()));
 		String[][] data = new String[sit.size()][5];
@@ -300,7 +360,7 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 			contor++;
 		}
 		JTable table = new JTable(data, columnNames);
-        table.setPreferredScrollableViewportSize(new Dimension(centerPanel.getWidth() - 5, centerPanel.getHeight() - 35));
+        table.setPreferredScrollableViewportSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() / 2));
         table.setFillsViewportHeight(true);
         table.setOpaque(false);
         table.getColumnModel().getColumn(0).setPreferredWidth(15);
@@ -311,12 +371,150 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
         	centerPanel.remove(scrollPane);
         }
         scrollPane = new JScrollPane(table);
-        scrollPane.setPreferredSize(new Dimension(centerPanel.getWidth() - 5, centerPanel.getHeight() - 35));
+        scrollPane.setPreferredSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() * 6 / 7));
         scrollPane.getViewport().setBackground(Color.DARK_GRAY);
         setScrollPane(scrollPane);
         centerPanel.add(scrollPane);
         
+        if(medieSemBttn != null) {
+        	centerPanel.remove(medieSemBttn);
+        }
+        if(medieSemLbl != null) {
+        	centerPanel.remove(medieSemLbl);
+        }
+        if(medieGenBttn != null) {
+        	centerPanel.remove(medieGenBttn);
+        }
+        if(medieGenLbl != null) {
+        	centerPanel.remove(medieGenLbl);
+        }
+        if(nrAbsBttn != null) {
+        	centerPanel.remove(nrAbsBttn);
+        }
+        if(nrAbsLbl != null) {
+        	centerPanel.remove(nrAbsLbl);
+        }
         
+        medieSemBttn = new JButton("Calculeaza-ti media pe acest semestru :");
+        medieSemBttn.setContentAreaFilled(false);
+        medieSemBttn.setForeground(Color.WHITE);
+        medieSemBttn.setActionCommand(CALCULEAZA_MEDIE_SEMESTRU);
+        medieSemBttn.addActionListener(new CustomActionListener());
+        centerPanel.add(medieSemBttn);
+        medieSemLbl = new BasicLabel("Media ta aici");
+        medieSemLbl.setForeground(Color.GRAY);
+        medieSemLbl.setPreferredSize(new Dimension(100, 30));
+        centerPanel.add(medieSemLbl);
+        
+        medieGenBttn = new JButton("Calculeaza-ti media generala :");
+        medieGenBttn.setContentAreaFilled(false);
+        medieGenBttn.setForeground(Color.WHITE);
+        medieGenBttn.setActionCommand(CALCULEAZA_MEDIE_GENERALA);
+        medieGenBttn.addActionListener(new CustomActionListener());
+        centerPanel.add(medieGenBttn);
+        medieGenLbl = new BasicLabel("Media ta aici");
+        medieGenLbl.setForeground(Color.GRAY);
+        centerPanel.add(medieGenLbl);
+        
+        centerPanel.revalidate();
+		centerPanel.repaint();
+	}
+	
+	private JButton nrAbsBttn = null;
+	private BasicLabel nrAbsLbl = null;
+	private int nrAbs = 0;
+	
+	public void setupAbsentePanel() {
+		JPanel centerPanel = super.getCenterPanel();
+		String[] columnNames = { "Materie", 
+								 "Absente", 
+								 "Total absente" };
+		TreeSet<Clasa> clase = Centralizator.getInstance().getClase();
+		String clasa = Centralizator.getInstance().getElevi().get(super.getUsername()).getClasa();
+		Clasa found = null;
+		for(Clasa c : clase) {
+			if(c.getID().equals(clasa)) {
+				found = c;
+				break;
+			}
+		}
+		if(found == null) {
+			return;
+		}
+		Catalog ctg = found.getCatalog();
+		TreeMap<Materie, SituatieMaterieBaza> sit = found.getCatalog().getMyMap().
+				get(Centralizator.getInstance().getElevi().get(getUsername()));
+		String[][] data = new String[sit.size()][3];
+		int contor = 0;
+		nrAbs = 0;
+		for(Map.Entry<Materie, SituatieMaterieBaza> entry : sit.entrySet()) {
+			data[contor][0] = "<html>" +
+					"<p><font size = \"5\">" + 
+					entry.getKey().getNume() + 
+					"</font></p></html>";
+			LinkedList<Absenta> absente = entry.getValue().getAbsente();
+			data[contor][1] = "<html>";
+			int blankLine = 0;
+			for(Absenta a : absente) {
+				DateFormat df = new SimpleDateFormat("yyyy-MMM-dd");
+				blankLine ++;
+				data[contor][1] += df.format(a.getDate().getTime()) + ", ";
+				if(blankLine == 10) {
+					blankLine = 0;
+					data[contor][1] += "<br>";
+				}
+			}
+			data[contor][1] += "</html>";
+			data[contor][2] = "<html><p><font color = \"blue\" size = \"5\">";
+			data[contor][2] += absente.size();
+			nrAbs += absente.size();
+			data[contor][2] += "</font></p></html>";
+			contor++;
+		}
+		JTable table = new JTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() / 2));
+        table.setFillsViewportHeight(true);
+        table.setOpaque(false);
+        for(int row = 0; row < table.getRowCount(); row++) {
+        	table.setRowHeight(row, 45);
+        }
+        if(scrollPane != null) {
+        	centerPanel.remove(scrollPane);
+        }
+        scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(centerPanel.getWidth() - 10, centerPanel.getHeight() * 6 / 7));
+        scrollPane.getViewport().setBackground(Color.DARK_GRAY);
+        setScrollPane(scrollPane);
+        centerPanel.add(scrollPane);
+        
+        if(medieSemBttn != null) {
+        	centerPanel.remove(medieSemBttn);
+        }
+        if(medieSemLbl != null) {
+        	centerPanel.remove(medieSemLbl);
+        }
+        if(medieGenBttn != null) {
+        	centerPanel.remove(medieGenBttn);
+        }
+        if(medieGenLbl != null) {
+        	centerPanel.remove(medieGenLbl);
+        }
+        if(nrAbsBttn != null) {
+        	centerPanel.remove(nrAbsBttn);
+        }
+        if(nrAbsLbl != null) {
+        	centerPanel.remove(nrAbsLbl);
+        }
+        
+        nrAbsBttn = new JButton("Calculeaza-ti numarul total de absente :");
+        nrAbsBttn.setContentAreaFilled(false);
+        nrAbsBttn.setForeground(Color.WHITE);
+        nrAbsBttn.setActionCommand(TOTAL_ABSENTE);
+        nrAbsBttn.addActionListener(new CustomActionListener());
+        centerPanel.add(nrAbsBttn);
+        nrAbsLbl = new BasicLabel("Numarul de absente aici");
+        nrAbsLbl.setForeground(Color.GRAY);
+        centerPanel.add(nrAbsLbl);
         
         centerPanel.revalidate();
 		centerPanel.repaint();
@@ -425,12 +623,12 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
         }  
     }
 	
-	private class Events implements ActionListener {
-
+	private class CustomActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			
 			String cmd = e.getActionCommand();
+			DecimalFormat myDecimalFormat = new DecimalFormat("#.00");
 			switch(cmd) {
 			case NOTE_SEMESTRUL_1 :
 				setupCatalogPanel(Semestru.SEMESTRUL_1);
@@ -438,9 +636,87 @@ public class ElevFrame extends UserFrameEquiped implements IUserFrameActions {
 			case NOTE_SEMESTRUL_2 :
 				setupCatalogPanel(Semestru.SEMESTRUL_2);
 				break;
+			case CALCULEAZA_MEDIE_SEMESTRU :
+				medieSemLbl.setText(myDecimalFormat.format(medieSem));
+				break;
+			case CALCULEAZA_MEDIE_GENERALA :
+				medieGenLbl.setText(myDecimalFormat.format(medieGen));
+				break;
+			case ABSENTE_SEMESTRU :
+				setupAbsentePanel();
+				break;
+			case TOTAL_ABSENTE :
+				nrAbsLbl.setText(myDecimalFormat.format(nrAbs));
+				break;
 			}
 			
 		}
+		
+	}
+	
+	private class ColegiTableModel extends AbstractTableModel {
+
+		TreeSet<Clasa> clase = Centralizator.getInstance().getClase();
+		String[] columnNames = { "ID Logare",
+				 "Nume si prenume", 
+				 "Medie semestrul 1", 
+				 "Medie semestrul 2",
+				 "Medie generala" };
+		String[][] data;
+		
+		public ColegiTableModel() {
+			Clasa found = null;
+			String clasa = Centralizator.getInstance().getElevi().get(getUsername()).getClasa();
+			for(Clasa c : clase) {
+				if(c.getID().equals(clasa)) {
+					found = c;
+					break;
+				}
+			}
+			if(found == null) {
+				return;
+			}
+			TreeSet<Elev> elevi = found.getElevi();
+			Catalog ctg = found.getCatalog();
+			
+			data = new String[elevi.size()][5];
+			int contor = 0;
+			DecimalFormat myDecimalFormat = new DecimalFormat("#.00");
+			for(Elev e : elevi) {
+				data[contor][1] = e.getNumeUtilizator().getNume() + " " + e.getNumeUtilizator().getPrenume();
+				data[contor][2] = myDecimalFormat.format(ctg.getMedieElevSemestru(e, Semestru.SEMESTRUL_1));
+				data[contor][3] = myDecimalFormat.format(ctg.getMedieElevSemestru(e, Semestru.SEMESTRUL_2));
+				data[contor][4] = myDecimalFormat.format(ctg.getMedieGeneralaElev(e));
+				data[contor][0] = e.getLoginID();
+				contor++;
+			}
+			
+		}
+		
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public int getRowCount() {
+			return data.length;
+		}
+
+		@Override
+		public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+		@Override
+        public Object getValueAt(int row, int col) {
+            return data[row][col];
+        }
+		
+		@Override
+		public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
 		
 	}
 }
